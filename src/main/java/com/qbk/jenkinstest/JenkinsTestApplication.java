4297ac7e0529807e51c3ffe91a2e4ecbc7b86c4e
quboka <quboka@telecomyt.com.cn>
2020-01-16 21:34:16 +0800

    1

---
+++ src/main/java/com/qbk/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,20 @@
+package com.qbk.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String get(){
+        return "get";
+    }
+}
